9f04b2ed1330ddcbc832504e585c3759bfb5d509
Temitayo Kuyoro <tayo28kuyoro@gmail.com>
2025-03-26 23:07:44 -0700

    beautiful home

--- app/components/SideNav.js
+++ app/components/SideNav.js
@@ -9,18 +9,15 @@ import styles from './SideNav.module.css'
 export default function SideNav() {
     const [sideOpen, setSideOpen] = useState(false);
     return (
-        <div className={sideOpen ? styles.sidenavopen : styles.sidenavclosed}>
-            <div>{sideOpen ? <button className={styles.sidenavbuttonclose} onClick={() => setSideOpen(false)}>
-                <Image src="/arrow-left.svg" width={20} height={20} alt="close menu" />
-            </button> :
-                <button className={styles.sidenavbuttonopen} onClick={() => setSideOpen(true)} >
-                    <Image src="/align-left.svg" width={20} height={20} alt="open menu" />
+        <div className={`${styles.sidenav} ${sideOpen ? styles.sidenavopen : styles.sidenavclosed}`}>
+            <div className={styles.sidenavbutton}>
+                <button onClick={() => setSideOpen(!sideOpen)}>
+                    <Image src={sideOpen ? '/arrow-left.svg' : '/align-left.svg'} width={20} height={20} alt={sideOpen ? 'close menu' : 'open menu'} />
                 </button>
-            }
             </div>
             <div className={styles.sidenavgrid}>
                 <Link href='/about'><div><p>About us</p></div></Link>
-               <Link href='/contact'><div><p>Contact us</p></div></Link>
+                <Link href='/contact'><div><p>Contact us</p></div></Link>
                 <Link href='/career'><div><p>Careers</p></div></Link>
             </div>
         </div>
